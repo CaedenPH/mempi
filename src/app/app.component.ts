b605bd9bd522e1a8e37a7c6bde90906f972f1c34
CaedenPH <caedenperelliharris@gmail.com>
2022-10-24 00:34:25 +0100

    fix: Add base code for pimem

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatExpansionPanel } from '@angular/material/expansion';
+import { pi } from './pi';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+export class AppComponent {
+  title = 'mempi';
+  nextNumbers: string = ""; 
+  isValid: boolean = true;
+  background: string = pi.replace(/(\r\n|\n|\r)/gm, "").split("").reverse().join("");
+
+  @ViewChild('input') input: ElementRef | undefined;
+  @ViewChild('nextNumbersPanel') nextNumbersPanel: MatExpansionPanel | undefined;
+
+  onInput(): void {
+    this.nextNumbersPanel?.close();
+
+    this.isValid = pi.includes(this.input?.nativeElement.value);
+  }
+
+  calculateNextNumbers(): void | string {
+    const index = pi.indexOf(this.input?.nativeElement.value);
+    if (index < 0) return this.nextNumbers = "Bz";
+   
+    const length = this.input?.nativeElement.value.length;
+    this.nextNumbers = pi.slice(index + length, index + length + 6); 
+  }
+}
+
