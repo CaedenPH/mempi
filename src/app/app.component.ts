04014626bca1dd351b1168cd505729f643ffaaf3
CaedenPH <caedenperelliharris@gmail.com>
2022-10-30 00:01:09 +0100

    feat: Timer function to speedpi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatExpansionPanel } from '@angular/material/expansion';
+import { Subscription, interval } from 'rxjs';
+
 import { pi } from './pi';
 
 @Component({
@@ -7,27 +9,55 @@ import { pi } from './pi';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  title = 'mempi';
-  nextNumbers: string = ""; 
-  isValid: boolean = true;
-  background: string = pi.repeat(3).split("").reverse().join("");
+export class AppComponent implements OnInit {
+  public title = 'mempi';
+  public nextNumbers: string = ""; 
+  public isValid: boolean = true;
+  public background: string = pi.repeat(3).split("").reverse().join("");
+  public timer: string = "0.0s";
+  public previous: string = "0.0s";
+  public mistakes: number = 0;
+  
+  private subscription: Subscription | undefined;
 
   @ViewChild('input') input: ElementRef | undefined;
   @ViewChild('nextNumbersPanel') nextNumbersPanel: MatExpansionPanel | undefined;
 
+  ngOnInit(): void {
+    this.subscription = interval(100)
+      .subscribe(x => {
+        if (this.input?.nativeElement.value) {
+          const count = Number.parseFloat(this.timer.slice(0, this.timer.length-1))+0.1;
+          let rounded = (Math.round(count * 100) / 100).toString();
+          if (rounded.length == 1) rounded += ".0"
+          this.timer = `${rounded}s`;
+        }
+      })
+  }
   onInput(): void {
     this.nextNumbersPanel?.close();
+    const value = this.input?.nativeElement.value;
 
+    if (!value) {
+      this.previous = this.timer != "0.0s" ? this.timer : "0.0s"
+      this.timer = "0.0s";
+      this.mistakes = 0;
+      return;
+    }
+    if (this.timer == "0.0s") {
+      this.timer = "0.0s"
+    }
+    
     this.isValid = pi.includes(this.input?.nativeElement.value);
+    if (!this.isValid) this.mistakes += 1;
   }
-
   calculateNextNumbers(): void | string {
     const index = pi.indexOf(this.input?.nativeElement.value);
     if (index < 0) return this.nextNumbers = "Bz";
    
     const length = this.input?.nativeElement.value.length;
-    this.nextNumbers = pi.slice(index + length, index + length + 6); 
+    const numbers = pi.slice(index + length, index + length + 6); 
+    this.nextNumbers = `${numbers.slice(0, 3)},${numbers.slice(3,6)}`
   }
 }
 
